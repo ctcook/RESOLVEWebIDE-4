227e8eefd2b588f1e221e1ed28070020092c642a
Chuck Cook <ctcook@g.clemson.edu>
2012-11-06 20:48:51 -0500

    Tweaked the URL generation for the web socket comile stuff.

--- public/javascripts/components.js
+++ public/javascripts/components.js
@@ -74,7 +74,7 @@ var Component = Backbone.RelationalModel.extend({
         var pathname = loc.pathname;
         pathname = pathname.substring(0,pathname.lastIndexOf("/"));
         //var url = "http://" + loc.host + (loc.pathname.length>1?loc.pathname+"/":loc.pathname) + "Components";
-        var url = "http://" + loc.host + (loc.pathname.length>1?loc.pathname+"/":loc.pathname) + "Components";
+        var url = "http://" + loc.host + (loc.pathname.length>1?pathname+"/":loc.pathname) + "Components";
         return url;
         //return this.collection.url() + '/Components';
     }
