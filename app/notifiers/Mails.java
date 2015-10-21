2ceca6632bfa2b45dba07c27dea4db8acc4400ae
Yu-Shan Sun <yushans@g.clemson.edu>
2015-10-21 14:29:32 -0400

    The sender information now depends on the request host

--- app/notifiers/Mails.java
+++ app/notifiers/Mails.java
@@ -16,7 +16,7 @@ public class Mails extends Mailer {
     public static void welcome(User user) {
         setSubject("Welcome to RESOLVE Web IDE");
         addRecipient(user.email);
-        setFrom("Clemson RSRG <do_not_reply@resolve.cs.clemson.edu>");
+        setFrom("Clemson RSRG <do_not_reply@" + Http.Request.current().host + ">");
         
         String link = Http.Request.current().getBase() + (String)Play.configuration.get("http.path") + "/";
         send(user, link);
@@ -26,7 +26,7 @@ public class Mails extends Mailer {
         String confirmationCode = user.confirmationCode;
         setSubject("RESOLVE Web IDE Registration Confirmation");
         addRecipient(user.email);
-        setFrom("Clemson RSRG <do_not_reply@resolve.cs.clemson.edu>");
+        setFrom("Clemson RSRG <do_not_reply@" + Http.Request.current().host + ">");
         
         String link = Http.Request.current().getBase() + (String)Play.configuration.get("http.path") + "/confirm?c_code=" + confirmationCode + "&email=" + user.email;
         send(user, link);
@@ -36,7 +36,7 @@ public class Mails extends Mailer {
         String confirmationCode = user.confirmationCode;
         setSubject("RESOLVE Web IDE Password Recovery");
         addRecipient(user.email);
-        setFrom("Clemson RSRG <do_not_reply@resolve.cs.clemson.edu>");
+        setFrom("Clemson RSRG <do_not_reply@" + Http.Request.current().host + ">");
         
         String link = Http.Request.current().getBase() + (String)Play.configuration.get("http.path") + "/reset?c_code=" + confirmationCode + "&email=" + user.email;
         send(user, link);
@@ -45,7 +45,7 @@ public class Mails extends Mailer {
     public static void resetPassword(User user) {
         setSubject("RESOLVE Web IDE Password Reset");
         addRecipient(user.email);
-        setFrom("Clemson RSRG <do_not_reply@resolve.cs.clemson.edu>");
+        setFrom("Clemson RSRG <do_not_reply@" + Http.Request.current().host + ">");
         send(user);
     }
 }
